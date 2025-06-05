347553c065d31b37a554498a88d6688bddd56568
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-06-05 23:05:09 +0200

    logo full
    full logo, need to upgrade more.
    Start with footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,7 +1,8 @@
 import "./Footer.css";
 
 export const Footer = () => `
-<div>
-    <p>&copy; 2025 To-Do List creada por Carlos Vidal y Yria Forjan. Todos los derechos reservados.</p>
+<div class="footer-div">
+    <img src="/icons/full-dootzy.png" alt="Logo" class="footer-logo"/>
+    <p>&copy; 2025 To-Do List created by Yria Forjan and Carlos Vidal. All rights reserved.</p>
 </div>
 `;
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { Footer } from "./components/layout/Footer/Footer.js";
 const init = () => {
   document.body.innerHTML += Header();
   Main("home");
-  /* document.body.innerHTML += Footer(); */
+  document.body.innerHTML += Footer(); 
 };
 
 document.addEventListener("DOMContentLoaded", () => {
