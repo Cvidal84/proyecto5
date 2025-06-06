a26587142566ed05d2d277c4845289ff37328d1c
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-06-06 16:55:36 +0200

    upgrade
    Aside calendar from librafy

--- src/components/DailyPanel/DailyPanel.js
+++ src/components/DailyPanel/DailyPanel.js
@@ -1,10 +1,23 @@
 import "./DailyPanel.css";
 
 export const DailyPanel = () => {
-    const aside = document.createElement("aside");
-    aside.id = "daily-panel";
-    aside.innerHTML = `
-        <p>Día, hora, clima...</p>
-    `;
-    return aside;
+  const aside = document.createElement("aside");
+  aside.id = "daily-panel";
+  aside.innerHTML = `
+    <p>Día, hora, clima...</p>
+    <div id='calendar'></div>
+  `;
+
+  // Inicializa el calendario cuando el aside ya está listo
+  setTimeout(() => {
+    const calendarEl = aside.querySelector('#calendar');
+    if (calendarEl) {
+      const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+      });
+      calendar.render();
+    }
+  }, 0);
+
+  return aside;
 };
