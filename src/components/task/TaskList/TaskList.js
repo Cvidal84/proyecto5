a37803201acce414ebe6d7e636b34d9d906be6a2
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-06-16 09:37:46 +0200

    fix problem
    on local storage

--- src/components/task/TaskList/TaskList.js
+++ src/components/task/TaskList/TaskList.js
@@ -128,7 +128,7 @@ export const TaskList = (listData, container) => {
             }
         });
 
-        localStorage.setItem("taskLists", JSON.stringify(updated));
+        localStorage.setItem("taskLists", JSON.stringify(currentLists));
     };
 
     // Añadir tareas ya guardadas
