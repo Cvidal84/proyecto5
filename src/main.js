a5db69fcce79b29cff4cd2c00cdfd7e95c06e470
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2025-06-19 20:33:35 +0200

    fix(pwa): reload app on service worker update to apply new content

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import { Header } from "./components/layout/Header/Header.js";
 import { toggleTheme } from "./components/layout/ThemeBtn/ThemeBtn.js";
+import { burgerMenu } from "./components/layout/Navbar/Navbar.js";
 import { Main } from "./components/layout/Main/Main.js";
 import { Footer } from "./components/layout/Footer/Footer.js";
 import { linkPage } from "./utils/linkPage.js";
-import { burgerMenu } from "./components/layout/Navbar/Navbar.js";
+import { setupSWUpdateListener } from "./utils/swUpdateHandler.js";
 
 const init = () => {
   document.body.innerHTML = Header();
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     init();
     toggleTheme();
     burgerMenu();
+    setupSWUpdateListener();
     requestAnimationFrame(() => {
       document.body.classList.add("loaded");
     });
---
+++ src/utils/swUpdateHandler.js
@@ -0,0 +1,7 @@
+export const setupSWUpdateListener = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+        window.location.reload();
+    });
+  }
+};
